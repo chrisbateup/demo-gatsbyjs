11cd9f25d5124dbe0ae99c8dbb1f1268c6963f53
Chris Bateup <github@chrisbateup.com>
2018-06-27 19:35:14 +1000

    Finish part four

---
+++ tutorial-part-four/gatsby-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  siteMetadata: {
+    title: `Pandas Eating Lots`,
+  },
+  plugins: [
+    `gatsby-plugin-glamor`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+  ],
+};
+  
